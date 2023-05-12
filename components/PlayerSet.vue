34841724bdc3b6282aa722e73642373f374f2545
Maxime Tytgat <mty@apik.cloud>
2023-05-12 08:41:42 +0200

    [WIP]

--- components/PlayerSet.vue
+++ components/PlayerSet.vue
@@ -2,6 +2,11 @@
   import { ref } from 'vue';
   import CardContainer from '@/components/CardContainer.vue';
 
+
+  const props = defineProps<{
+    cardData: CardData
+  }>()
+
   const container1Cards = ref([{id: 1, text: "AS"}, {id: 2, text: "2S"}]);
   const container2Cards = ref([{id: 3, text: "3S"}, {id: 4, text: "4S"}, {id: 5, text: "5S"}, {id: 6, text: "6S"}]);  
 </script>
@@ -9,9 +14,6 @@
 <template>
   <div class="player-set">
     <CardContainer class="card-container" :initialCards="container1Cards" />
-    <CardContainer class="card-container" :initialCards="container2Cards" />
-    <CardContainer class="card-container" />
-    <CardContainer class="card-container" />
   </div>
 </template>
 
